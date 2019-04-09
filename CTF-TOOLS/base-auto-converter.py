0f8da9b9d004ab330fa866170486aa275787b602
ZYA <zyazhb@yahoo.com>
2019-04-09 16:38:20 +0800

    Update base-auto-converter.py

--- CTF-TOOLS/base-auto-converter.py
+++ CTF-TOOLS/base-auto-converter.py
@@ -6,27 +6,28 @@ BY-ZYA
 import codecs
 import base64
 import re
-basestring = b"Vm0weGQxSXlSblJWV0d4WFlUSm9WRll3WkRSV01XeHlXa1pPYUZKc1NsWldSM1JQVmpGS2RHVkVRbFZXYkhCUVdWZHplRll4VG5OWGJGcFhaV3RhU1ZkV1kzaFRNVTVYVW01S2FGSnRhRzlVVm1oRFZWWmFjbHBFVWxSaVZrWTFWa2QwYTJGc1NuUlZiRkphWWtkU2RscFdXbXRXTVZaeVdrWndWMkV6UWpaV01uUnZWakZhZEZOc1dsaGlSMmhvVm1wT2IxTXhjRmhsUjBaWFlrZFNlVll5ZUVOV01rVjNZMFpTVjFaV2NGTmFSRVpEVld4Q1ZVMUVNRDA9="
+basestring = b"R1pDRE1SUldHTTNUSU5SV0c1QkRNUkJUR0UzRElOUlVHWkJUTU5KVklZM0RHTlpTRzQ0VE9NQlhHUVpUQU4yRQ=="
+
 while(1):
     base64_flag=0
     basestring = codecs.decode(basestring,"utf8")
-    print(basestring)
+    print(basestring+"\n")
     if '{' in basestring:
         break 
     for i in basestring:
         if(i.islower()):
             basestring = base64.b64decode(basestring)
-            # print "base64 encode"           
+            print("base64 decode:")
             base64_flag=1
             break
     if(base64_flag):
         continue
     elif(re.match('^[G-Z]',basestring)):
-        # print "base32 encode"       
+        print("base32 decode:")
         basestring=base64.b32decode(basestring)
         continue
     else:
-        # print "base16 encode"       
+        print("base16 decode:")
         basestring=base64.b16decode(basestring)
         continue
-    print (basestring)
+print ("-"*50+"\nPlain text: "+basestring)
